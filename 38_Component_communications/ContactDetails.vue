2757a4b21fd87d65cb07b7b933a380e39a20baa9
Sajib Kumar Barai <sajib60wab@gmail.com>
2023-05-03 20:59:18 +0600

    38. Component communications

---
+++ 38_Component_communications/ContactDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="contact-details">
+    <h1 class="contact-heading">Contact Details</h1>
+    <p><strong>Name: </strong>{{ name }}</p>
+    <p><strong>Website: </strong>{{ website }}</p>
+    <p><strong>Address: </strong>{{ address }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: { type: String, require: true },
+    website: { type: String, require: true },
+    address: { type: String, require: true },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.contact-details {
+  border: 1px solid gray;
+  margin: 11px 22px;
+  padding: 22px;
+}
+.contact-details {
+  color: rgb(0, 73, 107);
+}
+</style>
